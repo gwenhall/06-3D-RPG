701239041a3bf816759ab83f22c8e1af6ed99c7f
gwenhall <gwenhall@iu.edu>
2021-05-01 22:26:50 -0400

    Update

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,6 +10,8 @@ var max_speed = 4
 
 var mouse_sensitivity = 0.002
 
+var target = null
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -29,7 +31,8 @@ func _physics_process(_delta):
 	$AnimationTree.set("parameters/Idle_Run/blend_amount", current_speed/max_speed)
 	velocity = move_and_slide(velocity, Vector3.UP, true)
 
-
+	if Input.is_action_just_pressed("shoot") and target != null:
+		target.die()
 
 func _input(event):
 	if event is InputEventMouseMotion:
